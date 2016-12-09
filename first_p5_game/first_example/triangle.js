e4955f10d72ad2c30a03474bc9759ce62ada85d9
Joe Doe <svtgomez@gmail.com>
2016-12-08 20:55:31 -0800

    first

---
+++ first_p5_game/first_example/triangle.js
@@ -0,0 +1,18 @@
+class Triangle {
+    boolean isJunk;
+
+    Triangle(color) {
+        this.isJunk = false;
+    }
+
+    /*
+    void show() {
+        noStroke();
+        translate(width*0.2, height*0.5);
+        rotate(frameCount / 200.0);
+        //fill('blue', 200);
+        fill('green');
+        polygon(0, 0, 82, 3);
+    }
+    */
+}
